4730d1bf1f3209575733f6d3ec7a31ab545c3c14
Sean Conrad <sean.w.conrad@gmail.com>
2022-09-29 06:43:27 -0400

    client folder

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,13 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(({ request }) => request.destination === 'image',
+new CacheFirst({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0,200],
+    }),
+  ]
+})
+);
